c87a97b1b4429c6946205a235bb3174758adfd64
austin <austin.d.crow@gmail.com>
2020-03-01 12:13:35 -0800

    First Commit

---
+++ tests.py
@@ -0,0 +1,17 @@
+import unittest
+import task
+
+
+class TestCase(unittest.TestCase):
+
+	def test1(self):
+		expected = "success"
+		self.assertEqual(expected, task.firstrun())
+
+	def test2(self):
+		expected = "failure"
+		self.assertNotEqual(expected, task.firstrun())
+
+
+if __name__ == '__main__':
+	unittest.main()
