9f015c3100080d10cdf8b64988ca76b4035c0cc7
austin <austin.d.crow@gmail.com>
2020-03-01 12:58:11 -0800

    Fixing unittest for circle

--- task.py
+++ task.py
@@ -6,6 +6,6 @@ def computecirclearea(radius):
     return radius * radius * 3.14159
 
 
-def returnfirstandlast(inputlist):
-    n = len(inputlist)
-    return inputlist[0], inputlist[n - 1]
+#def returnfirstandlast(inputlist):
+ #   n = len(inputlist)
+  #  return inputlist[0], inputlist[n - 1]
--- tests.py
+++ tests.py
@@ -12,14 +12,15 @@ class TestCase(unittest.TestCase):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
-    def circleTest(self):
+    def test3(self):
         expected = 28.27431
         self.assertEqual(expected, task.computecirclearea(3))
 
-    def listTest(self):
-        expected = 1, 0
-        inputlist = [1, 4, 5, 6, 7, 8, 9, 0]
-        self.assertEqual(expected, task.returnfirstandlast(inputlist))
+    #def test4(self):
+     #   expected = 1, 0
+      #  inputlist = [1, 4, 5, 6, 7, 8, 9, 0]
+       # print(inputlist)
+        #self.assertEqual(expected, task.returnfirstandlast(inputlist))
 
 
 if __name__ == '__main__':
