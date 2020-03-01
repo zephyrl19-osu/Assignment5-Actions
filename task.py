75eabe8a234320e83d5d1cc65cc638b105d88b9d
austin <austin.d.crow@gmail.com>
2020-03-01 15:07:42 -0800

    fixing whitespace

--- task.py
+++ task.py
@@ -13,11 +13,11 @@ def firstandlast(inputlist):
 def timedates(date1, date2):
     # date input formate MMDDYYYY
     daydiff = 0
-    monthdays = [31,28,31,30,31,30,31,31,30,31,30,31]
+    monthdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if date1[2] == date2[2]:
         if date1[0] == date2[0]:
             daydiff = (date2[1] - date1[1])
     if date1[2] != date2[2]:
-        daydiff = 365*(date2[2]-date1[2])
+        daydiff = 365 * (date2[2] - date1[2])
 
     return daydiff
